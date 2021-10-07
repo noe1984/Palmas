8d9c36d5b9b7bfe5a40153125cd9aa7cfcfc5e98
Noe Gonzalez <palmas.inter@gmail.com>
2021-10-07 11:23:51 -0700

    responsive

--- src/components/ServicioTecnico.js
+++ src/components/ServicioTecnico.js
@@ -10,15 +10,15 @@ export const ServicioTecnico = () => {
         <div className="container-fluid " id='ServicioTecnico'>
             <h4 className='ServicioTecnico-title'>Servicio tecnico</h4>
             <div className='row'>
-                <ServicioTecnicoCard src={format} width='12rem' height='16rem'>
+                <ServicioTecnicoCard src={format} width='16rem' height='19rem'>
                     Formateo de computadoras, incluye sistema operativo y el Office mas reciente
                 </ServicioTecnicoCard>
 
-                <ServicioTecnicoCard src={programs} width='12rem' height='16rem'>
+                <ServicioTecnicoCard src={programs} width='16rem' height='19rem'>
                     Instalacion de distintos programas, como office, programas de diseño, drivers etc...
                 </ServicioTecnicoCard>
 
-                <ServicioTecnicoCard src={venta} width='12rem' height='16rem'>
+                <ServicioTecnicoCard src={venta} width='16rem' height='19rem'>
                     Venta de equipos de computo completos y tambien accesorios por separado
                 </ServicioTecnicoCard>
             </div>
