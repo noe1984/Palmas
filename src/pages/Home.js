71906f5ac9891f741ac40fc07a6937e96c19a430
noe <palmas.inter@gmail.com>
2023-05-25 17:26:27 -0700

    home video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './styles/Home.css'
+import video from '../videos/tech.mp4'
 
 export const Home = () => {
     return (
         <>
         <div className='cover-container'>
+            <video src={video} autoPlay loop muted />
             <h1>Internet Las Palmas</h1>
         </div>  
         
-        </>
+        </>  
     ) 
 } 
 
